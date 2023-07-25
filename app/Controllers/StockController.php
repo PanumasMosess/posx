072389c5d1f267d9ea750de17d126bd1893fd274
Panumas Srisook <Panumas.srisook@gmail.com>
2023-07-25 17:39:42 +0700

    update icon

---
+++ app/Controllers/StockController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controllers;
+
+date_default_timezone_set('Asia/Jakarta');
+
+use App\Controllers\BaseController;
+use Aws\S3\S3Client;
+
+class StockController extends BaseController
+{
+
+    public function __construct()
+    {
+        
+    }
+
+}
--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>POSX</title>
-    <link rel="icon" href="<?php echo base_url('img/mini_logo.png" type="image/png'); ?>" />
+    <link rel="icon" href="<?php echo base_url('img/mini_logo.png'); ?>" type="image/png" />
 
     <link rel="stylesheet" href="<?php echo base_url('css/bootstrap1.min.css'); ?>" />
 
@@ -13,7 +13,7 @@
 
     <link rel="stylesheet" href="<?php echo base_url('vendors/niceselect/css/nice-select.css'); ?>" />
 
-    <link rel="stylesheet" href="<?php echo base_url('vendors/owl_carousel/css/owl.carousel.css'); ?>'); ?>" />
+    <link rel="stylesheet" href="<?php echo base_url('vendors/owl_carousel/css/owl.carousel.css'); ?>" />
 
     <link rel="stylesheet" href="<?php echo base_url('vendors/gijgo/gijgo.min.css'); ?>" />
 
