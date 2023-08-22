b69fa6c1c786a4cf2aa13863c2186e460466d92d
Panumas Srisook <Panumas.srisook@gmail.com>
2023-08-22 16:57:09 +0700

    update test table

--- app/Views/order/order_pos.php
+++ app/Views/order/order_pos.php
@@ -3,42 +3,30 @@
         <div class="row justify-content-center">
             <div class="col-12">
                 <div class="white_card mb_30">
-                    <!-- <div class="white_card_header">
+                    <div class="white_card_header">
                         <div class="bulder_tab_wrapper">
                             <ul class="nav" id="stockTab" role="tablist">
                                 <li class="nav-item">
-                                    <a class="nav-link active" id="Order-tab" data-bs-toggle="tab" href="#Order" role="tab" aria-controls="Order" aria-selected="true">รายการสินค้า</a>
+                                    <a class="nav-link active" id="Order-tab" data-bs-toggle="tab" href="#Order" role="tab" aria-controls="Order" aria-selected="true">จัดโต๊ะ</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " id="Order-tab" data-bs-toggle="tab" href="#" role="tab" aria-controls="Order" aria-selected="true">รายการสินค้า</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link " id="Order-tab" data-bs-toggle="tab" href="#" role="tab" aria-controls="Order" aria-selected="true">รายการสินค้า</a>
                                 </li>
                             </ul>
                         </div>
-                    </div> -->
+                    </div>
                     <div class="white_card_body">
-                        <!-- <div class="tab-content" id="stockTabContent">
+                        <div class="tab-content" id="stockTabContent">
                             <div class="tab-pane fade show active" id="Order" role="tabpanel" aria-labelledby="Order-tab">
                                 <div class="col-lg-12 col-xl-12 col-md-12">
-                                    <div class="justify-content-end d-flex mb-2"> <a href="javascript:void(0);" class="white_btn3 mb-2" onclick="openModalAddOrder();"><i class="ti-plus"></i>&nbsp;&nbsp;เพิ่มราการสินค้า</a></div>
-                                    <div class="QA_section">
-                                        <div class="QA_table mb_30">
-                                            <table class="table lms_table_active3" id="orderTable">
-                                                <thead>
-                                                    <tr>
-                                                        <th>No.</th>
-                                                        <th>สินค้า</th>
-                                                        <th>หมวดหมู่สินค้า</th>
-                                                        <th>ราคา / หน่วย</th>
-                                                        <th>Last Update</th>
-                                                        <th>Action</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
+                                    <div class="draggable"></div>
+                                    <div class="draggable"></div>
                                 </div>
                             </div>
-                        </div> -->
+                        </div>
                     </div>
                 </div>
             </div>
