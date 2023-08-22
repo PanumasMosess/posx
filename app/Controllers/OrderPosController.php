a8eaf2881b927b0fe8b9331591efef56f6863d3a
nuttapong <nuttapong.ep@gmail.com>
2023-08-22 13:39:26 +0700

    OrderPos

---
+++ app/Controllers/OrderPosController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+date_default_timezone_set('Asia/Jakarta');
+
+use App\Controllers\BaseController;
+
+class OrderPosController extends BaseController
+{
+
+    public function __construct()
+    {
+        // Model Order
+        $this->OrderModel = new \App\Models\OrderModel();
+        // Model Stock
+        $this->StockModel = new \App\Models\StockModel();
+    }
+
+    public function index()
+    {
+        $data['content'] = 'order/order_pos';
+        $data['title'] = 'OrderPos';
+        $data['css_critical'] = '<link rel="stylesheet" href="' . base_url('css/err_style.css') . '" />';
+        $data['js_critical'] = ' 
+            <script src="' . base_url('/js/notify/js/notifIt.js') . '"></script>
+            <script src="' . base_url('/js/base64/jquery.base64.min.js') . '"></script>
+            <script src="' . base_url('/js/orders/order_pos.js?v=' . time()) . '"></script>    
+        ';
+        echo view('/app', $data);
+    }
+}
