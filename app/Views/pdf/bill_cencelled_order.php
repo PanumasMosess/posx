8b8cf0b9aa9a2ce3a6b2928a03dd26de5f3bf75f
nuttapong <nuttapong.ep@gmail.com>
2023-09-22 18:37:13 +0700

    PDF

---
+++ app/Views/pdf/bill_cencelled_order.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en" data-layout="horizontal" data-hor-style="hor-hover" data-logo="centerlogo">
+
+<body>
+    <table>
+        <tr>
+            <th style="font-size: 20px;"></th>
+        </tr>
+        <tr>
+            <th width="99%" style="text-align: center;font-size: 15px;">***** Original Copy to Cashier *****</th>
+        </tr>
+        <tr>
+            <th style="font-size: 5px;"></th>
+        </tr>
+        <tr>
+            <th width="13%">Date : </th>
+            <th width="86%"><?php echo date('d/m/Y H:i:s') ?></th>
+        </tr>
+        <tr>
+            <th width="8%">by : </th>
+            <th width="91%">Test</th>
+        </tr>
+        <tr>
+            <th style="font-size: 3px;"></th>
+        </tr>
+        <tr>
+            <th width="99%" style="text-align: center;font-size: 15px;font-weight: bold;">Table 1 (Cancelled)</th>
+        </tr>
+        <tr>
+            <th width="99%" style="font-size: 15px;">Cancelled item</th>
+        </tr>
+        <tr>
+            <th width="99%" style="font-size: 3px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
+        </tr>
+        <tr>
+            <th style="font-size: 5px;"></th>
+        </tr>
+        <tr>
+            <th width="10%" style="text-align: center; font-size: 20px;"><s>1</s></th>
+            <th width="89%" style="font-size: 20px;"><s>ก๋วยเตี๋ยวหมูน้ำตก</s></th>
+        </tr>
+        <tr>
+            <th width="10%"></th>
+            <th width="3%" style="font-size: 18px;">-</th>
+            <th width="86%" style="font-size: 18px;"><s>ไม่ใส่ผัก</s></th>
+        </tr>
+        <tr>
+            <th width="10%" style="text-align: center;font-size: 20px;"><s>1</s></th>
+            <th width="89%" style="font-size: 20px;"><s>น้ำเปล่า</s></th>
+        </tr>
+        <tr>
+            <th width="10%" style="text-align: center;font-size: 20px;"><s>2</s></th>
+            <th width="89%" style="font-size: 20px;"><s>น้ำแข็ง(แก้ว)</s></th>
+        </tr>
+        <tr>
+            <th width="99%" style="font-size: 5px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
+        </tr>
+        <tr>
+            <th style="font-size: 10px;"></th>
+        </tr>
+        <tr>
+            <th width="17%">Reason : </th>
+            <th width="82%"></th>
+        </tr>
+        <tr>
+            <th width="11%">Sign : </th>
+            <th width="88%"></th>
+        </tr>
+        <tr>
+            <th style="font-size: 10px;"></th>
+        </tr>
+        <tr>
+            <th width="99%" style="text-align: center;font-size: 15px;">***** Original Copy to Cashier *****</th>
+        </tr>
+    </table>
+</body>
+
+</html>
