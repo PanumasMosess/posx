2c8a60516f5dc115e9270412a4014e07f0edc9e1
Panumas Srisook <Panumas.srisook@gmail.com>
2023-11-13 17:31:32 +0700

    update

--- public/js/orders/order_customer.js
+++ public/js/orders/order_customer.js
@@ -51,24 +51,24 @@ function findPrinters(printer, name_pdf) {
   ];
   qz.print(config, data)
     .then(function (data) {
-      // deletefilePDF(name_pdf);
-      $.ajax({
-        url:
-          `${serverUrl}/order/update_order_print_log/` +
-          response.order_customer_code,
-        method: "get",
-        success: function (res) {
-          // การสำเร็จ
-          
-        },
-        error: function (error) {
-          // เกิดข้อผิดพลาด
-        },
-      });
-     
+      // deletefilePDF(name_pdf);  
     })
     .catch(function (e) {
       console.error(e);
+    });
+
+    $.ajax({
+      url:
+        `${serverUrl}/order/update_order_print_log/` +
+        response.order_customer_code,
+      method: "get",
+      success: function (res) {
+        // การสำเร็จ
+        
+      },
+      error: function (error) {
+        // เกิดข้อผิดพลาด
+      },
     });
 }
 
