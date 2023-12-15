6c5dad2d80e21d15ca831d6eddd487479aa3ed5d
nuttapong <nuttapong.ep@gmail.com>
2023-12-15 18:51:04 +0700

    edit font size pdf

--- app/Views/pdf/bill.php
+++ app/Views/pdf/bill.php
@@ -6,22 +6,22 @@
         <tr>
             <th width="100%" style="text-align:center;">
                 <img src="https://app.posx.co/img/POSX_2.png" style="height:60px;width:180px;">
-                 <!-- <img src="https://app.posx.co/img/no-image-available.jpg" style="height:60px;width:180px;"> -->
+                <!-- <img src="https://app.posx.co/img/no-image-available.jpg" style="height:60px;width:180px;"> -->
             </th>
         </tr>
         <tr>
             <th style="font-size: 2px;"></th>
         </tr>
         <tr>
-            <th width="15%">Date : </th>
-            <th width="84%"><?php echo date('d/m/Y H:i:s') ?></th>
+            <th width="17%">Date : </th>
+            <th width="82%"><?php echo date('d/m/Y H:i:s') ?></th>
         </tr>
         <tr>
-            <th width="19%">Cashier : </th>
-            <th width="80%"><?php echo session()->get('username'); ?></th>
+            <th width="25%">Cashier : </th>
+            <th width="74%"><?php echo session()->get('username'); ?></th>
         </tr>
         <tr>
-            <th style="font-size: 2px;"></th>
+            <th style="font-size: 5px;"></th>
         </tr>
         <tr>
             <th width="99%" style="text-align: center;font-size: 13px;">**** PREVIEW ****</th>
@@ -34,8 +34,8 @@
         </tr>
         <tr>
             <th width="10%"></th>
-            <th width="60%">items</th>
-            <th width="29%" style="text-align: right;">Price</th>
+            <th width="57%">items</th>
+            <th width="32%" style="text-align: right;">Price</th>
         </tr>
         <tr>
             <th width="99%" style="font-size: 2px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
@@ -47,8 +47,11 @@
             $price = $orderlist->order_customer_pcs * $orderlist->order_price; ?>
             <tr>
                 <th width="10%" style="text-align: center;"><?php echo $orderlist->order_customer_pcs ?></th>
-                <th width="60%"><?php echo $orderlist->order_customer_ordername ?></th>
-                <th width="29%" style="text-align: right;"><?php echo number_format($price, 2) ?></th>
+                <th width="57%"><?php echo $orderlist->order_customer_ordername ?></th>
+                <th width="32%" style="text-align: right; font-size: 12px;"><?php echo number_format($price, 2) ?></th>
+            </tr>
+            <tr>
+                <th style="font-size: 2px;"></th>
             </tr>
         <?php } ?>
         <!-- <tr>
@@ -65,14 +68,14 @@
             <th width="99%" style="font-size: 1px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
         </tr>
         <tr>
-            <th style="font-size: 2px;"></th>
+            <th style="font-size: 3px;"></th>
         </tr>
         <tr>
             <th width="99%"><?php echo $summary->order_pcs_sum ?> Items</th>
         </tr>
         <tr>
-            <th width="70%" style="font-size: 18px;font-weight: bold;">Grand Total</th>
-            <th width="29%" style="text-align: right;font-size: 18px;font-weight: bold;"><?php echo number_format($summary->order_price_sum, 2) ?></th>
+            <th width="60%" style="font-size: 15px;font-weight: bold;">Grand Total</th>
+            <th width="39%" style="text-align: right;font-size: 15px;font-weight: bold;"><?php echo number_format($summary->order_price_sum, 2) ?></th>
         </tr>
         <tr>
             <th width="99%" style="font-size: 2px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
@@ -81,7 +84,7 @@
             <th width="99%" style="font-size: 1px; border-bottom-style: solid;border-bottom-color: #BEBEBE"></th>
         </tr>
         <tr>
-            <th style="font-size: 3px;"></th>
+            <th style="font-size: 5px;"></th>
         </tr>
         <tr>
             <th width="99%" style="text-align: center;font-size: 13px;">**** PREVIEW ****</th>
